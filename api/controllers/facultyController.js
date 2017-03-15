3ae8a09b201755bde2f28b4e2f44a1d48e87a366
devam996 <devamnarkar96@gmail.com>
2017-03-15 13:07:53 +0530

    Faculty Regis

--- api/controllers/facultyController.js
+++ api/controllers/facultyController.js
@@ -28,7 +28,7 @@ var facultyController = function (Faculty, Registration) {
 				throwError(response, error, 500, 'Internal Server error', 'Faculty Register');
 			} else {
 				var token;
-				token = faculty.generateJwt();
+				token = request.faculty.generateJwt();
 				response.status(200);
 				response.json({
 					"token": token,
