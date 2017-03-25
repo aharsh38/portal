c111b75e2c0720522b6e8fc67e340e9064da1212
devam996 <devamnarkar96@gmail.com>
2017-03-25 19:55:19 +0000

    server

--- public/scripts/main/fcApp.js
+++ public/scripts/main/fcApp.js
@@ -554,6 +554,63 @@
 	'use strict';
 
 	angular
+		.module('fct.api')
+		.factory('authInterceptor', authInterceptor);
+
+	authInterceptor.$inject = ['$window', '$q', '$location'];
+
+	function authInterceptor($window, $q, $location) {
+		var service = {
+			request: request,
+			requestError: requestError,
+			response: response,
+			responseError: responseError
+		};
+
+		return service;
+
+		function getToken() {
+			if ($window.localStorage['auth-token']) {
+				return $window.localStorage['auth-token'];
+			} else {
+				return null;
+			}
+		}
+
+		function request(config) {
+			var token = getToken();
+			if (token !== null) {
+				var authHead = 'Bearer ' + token;
+				config.headers['Authorization'] = authHead;
+			}
+			// console.log(config);
+			return config;
+		}
+
+		function requestError(rejection) {
+			// console.log("Request Rejection",rejection);
+			return $q.reject(rejection);
+		}
+
+		function response(response) {
+			// console.log("response",response);
+			return response || $q.when(response);
+		}
+
+		function responseError(rejection) {
+			// console.log("response rejection", rejection);
+			if (rejection.status == 403) {
+				$location.path('/login');
+			}
+			return $q.reject(rejection);
+		}
+	}
+})();
+
+(function () {
+	'use strict';
+
+	angular
 	  .module('fct.core')
 	  .factory('eventService', eventService);
 
@@ -1179,63 +1236,6 @@
 	'use strict';
 
 	angular
-		.module('fct.api')
-		.factory('authInterceptor', authInterceptor);
-
-	authInterceptor.$inject = ['$window', '$q', '$location'];
-
-	function authInterceptor($window, $q, $location) {
-		var service = {
-			request: request,
-			requestError: requestError,
-			response: response,
-			responseError: responseError
-		};
-
-		return service;
-
-		function getToken() {
-			if ($window.localStorage['auth-token']) {
-				return $window.localStorage['auth-token'];
-			} else {
-				return null;
-			}
-		}
-
-		function request(config) {
-			var token = getToken();
-			if (token !== null) {
-				var authHead = 'Bearer ' + token;
-				config.headers['Authorization'] = authHead;
-			}
-			// console.log(config);
-			return config;
-		}
-
-		function requestError(rejection) {
-			// console.log("Request Rejection",rejection);
-			return $q.reject(rejection);
-		}
-
-		function response(response) {
-			// console.log("response",response);
-			return response || $q.when(response);
-		}
-
-		function responseError(rejection) {
-			// console.log("response rejection", rejection);
-			if (rejection.status == 403) {
-				$location.path('/login');
-			}
-			return $q.reject(rejection);
-		}
-	}
-})();
-
-(function () {
-	'use strict';
-
-	angular
 		.module('fct.core')
 		.factory('fctToast', fctToast);
 
@@ -1508,6 +1508,58 @@
 
 	angular
 		.module('fct.core')
+		.controller('ConfirmRegistrationModalController', ConfirmRegistrationModalController);
+
+	ConfirmRegistrationModalController.$inject = ['$mdDialog', 'registration', 'facultyService'];
+
+	function ConfirmRegistrationModalController($mdDialog, registration, facultyService) {
+		var vm = this;
+		vm.registration = registration;
+		vm.registrationButtonClicked = false;
+
+		angular.extend(vm, {
+			confirmData: confirmData,
+			hide: hide,
+			cancel: cancel
+		});
+
+		function confirmData() {
+			if (vm.registrationButtonClicked) {
+				event.preventDefault();
+			} else {
+				vm.registrationButtonClicked = true;
+			}
+
+			return facultyService.confirmRegistration(registration)
+				.then(confirmRegistrationSuccess)
+				.catch(confirmRegistrationFailure);
+		}
+
+		function confirmRegistrationSuccess(response) {
+			console.log(response);
+			hide(response);
+		}
+
+		function confirmRegistrationFailure(error) {
+			$mdDialog.cancel(error);
+		}
+
+		function hide(response) {
+			$mdDialog.hide(response);
+		}
+
+		function cancel() {
+			$mdDialog.cancel();
+		}
+	}
+
+})();
+
+(function () {
+	'use strict';
+
+	angular
+		.module('fct.core')
 		.controller('AddStudentController', AddStudentController);
 
 	AddStudentController.$inject = ['$http', 'facultyService', '$rootScope', 'fctToast'];
@@ -1590,58 +1642,6 @@
 			fctToast.showToast('Error!! Try Again');
 		}
 	}
-})();
-
-(function () {
-	'use strict';
-
-	angular
-		.module('fct.core')
-		.controller('ConfirmRegistrationModalController', ConfirmRegistrationModalController);
-
-	ConfirmRegistrationModalController.$inject = ['$mdDialog', 'registration', 'facultyService'];
-
-	function ConfirmRegistrationModalController($mdDialog, registration, facultyService) {
-		var vm = this;
-		vm.registration = registration;
-		vm.registrationButtonClicked = false;
-
-		angular.extend(vm, {
-			confirmData: confirmData,
-			hide: hide,
-			cancel: cancel
-		});
-
-		function confirmData() {
-			if (vm.registrationButtonClicked) {
-				event.preventDefault();
-			} else {
-				vm.registrationButtonClicked = true;
-			}
-
-			return facultyService.confirmRegistration(registration)
-				.then(confirmRegistrationSuccess)
-				.catch(confirmRegistrationFailure);
-		}
-
-		function confirmRegistrationSuccess(response) {
-			console.log(response);
-			hide(response);
-		}
-
-		function confirmRegistrationFailure(error) {
-			$mdDialog.cancel(error);
-		}
-
-		function hide(response) {
-			$mdDialog.hide(response);
-		}
-
-		function cancel() {
-			$mdDialog.cancel();
-		}
-	}
-
 })();
 
 (function () {
